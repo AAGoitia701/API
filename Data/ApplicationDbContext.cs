c54695a32dc9af62589429a83160f2248aa837ad
=AAGoitia701 <aagoitia701@gmail.com>
2024-10-22 13:25:17 +0200

    Created database.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }    
+    }
+}
