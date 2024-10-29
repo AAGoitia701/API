58af879bfa81dd443306fb23ec6e0bf5b58bab92
=AAGoitia701 <aagoitia701@gmail.com>
2024-10-29 12:27:47 +0100

    Added pagination using the QueryObject on GetAll();

--- Data/Repository/StockRepository.cs
+++ Data/Repository/StockRepository.cs
@@ -67,7 +67,9 @@ namespace API.Repository
 
             }
 
-            return await stocks.ToListAsync();  
+            var skipNumber = (query.PageNumber -1) * query.PageSize;
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();  
         }
 
         public async Task<Stock> GetOneByIdAsync(int id)
